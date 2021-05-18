55f7824c806a3e46713876eba97031929f7d08e8
SKY <SKY@110.9.126.173>
2021-05-18 15:51:18 +0900

    ㅇㅇㅇ

---
+++ dfdas.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class dfdas {
+
+	public static void main(String[] args) {
+		Scanner Scanner = new Scanner(System.in);
+		System.out.println(("Ã¹ ¹øÂ° ÀÔ·Â °ª:"));
+		int first = Scanner.nextInt();
+		System.out.println(first);
+		System.out.println(("µÎ ¹øÂ° ÀÔ·Â °ª:"));
+		int second = Scanner.nextInt();
+		System.out.println(second);
+		System.out.println(" µ¡¼À :"+(first+second));
+		System.out.println(" »¬¼À :"+(first-second));
+		System.out.println(" °ö¼À :"+(first*second));
+		System.out.println(" ³ª´°¼À :"+(first/second));
+	}
+}
+
+
+
+
