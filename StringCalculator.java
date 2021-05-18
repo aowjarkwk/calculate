2fb0c7a09e43fd1dcd6a941b77c9098b50137792
SKY <SKY@110.9.126.173>
2021-05-18 17:21:03 +0900

    문자열로 계산

---
+++ StringCalculator.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class StringCalculator {
+	public static void main(String[]args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("계산할 값 입력 : ");
+		
+		String value = scanner.nextLine();
+		System.out.println("입력 값 :" + value);
+		
+		String[] values = value.split(" ");
+		
+		int first = Integer.parseInt(values[0]);
+		
+		int result = first;
+		for (int i = 1;i<values.length; i = i + 2) {
+			String symbol = values[i];
+			System.out.println("symbol : "+ symbol);
+			int second = Integer.parseInt(values[i+1]);
+		result = Calculate.calculate(result, symbol, second);
+		}
+		dfdas.print(result);
+	}
+}
