d94ca4d1cc8688b4fbdb20d35012830963d868f0
SKY <SKY@110.9.126.173>
2021-05-18 16:15:34 +0900

    종료

--- dfdas.java
+++ dfdas.java
@@ -6,21 +6,36 @@ public class dfdas {
 		Scanner Scanner = new Scanner(System.in);
 		System.out.println(("첫 번째 입력 값 : "));
 		int first = Scanner.nextInt();
-		System.out.println(("사칙연산 입력 : "));
-		String symbol = Scanner.next();
-		System.out.println(("두 번째 입력 값 : "));
-		int second = Scanner.nextInt();
-	if(symbol.equals("+")) {
-		System.out.println("덧셈:" + (first+second));
-	}else if(symbol.equals("-")) {
-		System.out.println("뺄셈:" + (first-second));
-	}else if(symbol.equals("*")) {
-		System.out.println("곱셈:"+(first*second));
-	}else if(symbol.equals("/")) {
-		System.out.println("나눗셈:"+(first/second));
-	}else {
-		System.out.println("올바른 사칙연산 값을 입력해주세요.");
-	}
+		int result=first;
+		
+		
+		
+		while(true) {
+		
+			System.out.println(("사칙연산 입력 : "));
+			String symbol = Scanner.next();
+			if (symbol.equals("quit")) {
+				System.out.println("최종 결과 값 : "+result);
+				break;}
+			System.out.println(("두 번째 입력 값 : "));
+			int second = Scanner.nextInt();	
+			if(symbol.equals("+")) {
+				result=result+second;
+				System.out.println("덧셈:" + result);
+			}else if(symbol.equals("-")) {
+				result=result-second;
+				System.out.println("뺄셈:" + result);
+			}else if(symbol.equals("*")) {
+				result=result*second;
+				System.out.println("곱셈:"+result);
+			}else if(symbol.equals("/")) {
+				result=result/second;
+				System.out.println("나눗셈:"+result);
+			}else {
+				System.out.println("올바른 사칙연산 값을 입력해주세요.");
+			}
+		}
+	
 		
 	}
 }
